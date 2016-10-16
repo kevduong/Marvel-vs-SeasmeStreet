c1f3a6d15157aa06bc63ce16ed4a58748969ca1b
Kevin Duong <duong.kev@gmail.com>
2016-10-15 21:06:15 -0700

    pseudocode for js

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+//Global Variable - Declare variables to be accessed on any function
+    //1: Game needs player, cpu, hp, attack value, scores for players and cpu
+    //2: Create an array or object for characters? Refer to Cobweb activity...
+
+// Setting function before the fight
+    
+//Set values. Refer to calculator assignment.    
+    
+//Loop - Add HP and Name variable to characters
+      
+//Messages and Buttons
+
+// Start Game
+
+// Pick a character
+
+// Player clicks 'btnAttack' to reduce HP of CPU.
+// CPU attacks back
+
+//Conditions for defeating first enemy, winning by defeating all three, losing, and tie
+
+//Results
+
+
+// Play/Pause Song //
